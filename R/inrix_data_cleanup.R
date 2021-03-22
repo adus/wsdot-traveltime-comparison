8133fc2f22b09a9d9238c42afdf8f5f256f165e1
leetam <leetam@pdx.edu>
2021-03-22 10:46:26 -0700

    clean-up i205 nb airport segment

--- R/inrix_data_cleanup.R
+++ R/inrix_data_cleanup.R
@@ -59,4 +59,60 @@ nb_i5_jantzen_tt <- nb_i5_jantzen %>%
 
 # Save hourly corridor tt df
 saveRDS(nb_i5_jantzen_tt, "data/nb_i5_jantzen_tt_inrix.rds")
-            
+
+#### I-205 NB Airport-Padden ####
+# Read-in data
+i205_airport_raw <- read.csv("data/inrix/i205-airport-padden-inrix.csv", stringsAsFactors = F)
+i205_airport_tmc <- read.csv("data/inrix/i205-airport-padden-tmc-id.csv", stringsAsFactors = F)
+
+# NB I205 TMC road order
+nb_i205_airport_tmc <- i205_airport_tmc %>%
+  filter(direction == "NORTHBOUND") %>%
+  arrange(start_latitude, start_longitude) %>%
+  mutate(segment_order = 1:n())
+
+# Create tmc and road segment df
+nb_i205_airport_route <- nb_i205_airport_tmc %>%
+  select(tmc, segment_order)
+
+# Filter raw data by nb tmc codes
+nb_i205_airport_raw <- i205_airport_raw %>%
+  filter(tmc_code %in% nb_i205_airport_tmc$tmc)
+
+# Plot confidence score by cvalue, fill by tmc_code
+conf_dist <- nb_i205_airport_raw %>%
+  ggplot(aes(x = tmc_code, fill = as.character(confidence_score))) +
+  geom_bar(stat = "count")
+conf_dist
+
+# Filter raw data by nb tmc codes, and:
+#  - Confidence score = 30
+nb_i205_airport <- i205_airport_raw %>%
+  filter(tmc_code %in% nb_i205_airport_tmc$tmc,
+         confidence_score == 30)
+
+# Histogram of cvalues
+c_value_hist <- nb_i205_airport %>%
+  ggplot(aes(x = cvalue)) +
+  geom_histogram(aes(y = ..count../sum(..count..))) +
+  facet_wrap(. ~ tmc_code)
+c_value_hist
+
+# Filter out c_values < 75 and NA's
+nb_i205_airport <- nb_i205_airport %>%
+  filter(cvalue > 75)
+
+# Calculate hourly average tt per segment
+nb_i205_airport_tt <- nb_i205_airport %>%
+  mutate(timestamp_hrly = floor_date(ymd_hms(measurement_tstamp), "hour")) %>%
+  group_by(tmc_code, timestamp_hrly) %>%
+  summarise(hourly_tt_sec = mean(travel_time_seconds)) %>%
+  group_by(timestamp_hrly) %>%
+  summarise(corridor_tt_min = sum(hourly_tt_sec)/60,
+            num_segments = n()) %>%
+  filter(num_segments == 13) %>%
+  select(datetime = timestamp_hrly, traveltime = corridor_tt_min) %>%
+  mutate(source = "INRIX")
+
+# Save hourly corridor tt df
+saveRDS(nb_i205_airport_tt, "data/nb_i205_airport_tt_inrix.rds")
