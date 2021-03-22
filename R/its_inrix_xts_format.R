ebe020132d2bcbeba96d4973ba49037615486eea
leetam <leetam@pdx.edu>
2021-03-22 12:05:16 -0700

    create i205 sb padden-airport xts tibble

--- R/its_inrix_xts_format.R
+++ R/its_inrix_xts_format.R
@@ -65,3 +65,34 @@ nb_i205_airport_tt_xts <- as.xts(nb_i205_airport_tt)
 
 ## Save xts tibble ----
 saveRDS(nb_i205_airport_tt_xts, "data/nb_i205_airport_tt_xts.rds")
+
+# SB I-205 Padden-Airport segment ----
+## Read-in INRIX and ITS data ----
+sb_i205_padden_tt_inrix <- readRDS("data/sb_i205_padden_tt_inrix.rds")
+sb_i205_padden_tt_its <- read.csv("data/portal/i205-padden-airport-portal.csv", stringsAsFactors = F)
+
+## Format ITS data ----
+sb_i205_padden_tt_its$time <- ymd_hms(sb_i205_padden_tt_its$time)
+sb_i205_padden_tt_its <- sb_i205_padden_tt_its %>%
+  select(datetime = time, traveltime = travel.time) %>%
+  mutate(source = "ITS")
+
+## Create xts df for dygraph ----
+sb_i205_padden_tt <- data.frame(
+  datetime = seq(
+    from = as.POSIXct("2019-02-03 00:00:00", tz = "America/Los_Angeles"),
+    to = as.POSIXct("2019-03-03 00:00:00", tz = "America/Los_Angeles"),
+    by = "hour"
+  )
+) %>%
+  left_join(sb_i205_padden_tt_its, by = "datetime") %>%
+  select(datetime, ITS = traveltime) %>%
+  left_join(sb_i205_padden_tt_inrix, by = "datetime") %>%
+  select(datetime, ITS, INRIX = traveltime)
+
+sb_i205_padden_tt <- column_to_rownames(sb_i205_padden_tt, var = "datetime")
+
+sb_i205_padden_tt_xts <- as.xts(sb_i205_padden_tt)
+
+## Save xts tibble ----
+saveRDS(sb_i205_padden_tt_xts, "data/sb_i205_padden_tt_xts.rds")
