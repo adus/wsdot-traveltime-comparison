442ef6f2a79a19fcbb96d3f80f538368f0452f01
leetam <leetam@pdx.edu>
2021-03-23 17:00:51 -0700

    finish format sr14 eb/wb data

--- R/inrix-its-SR14-cleanup-xts-format.R
+++ R/inrix-its-SR14-cleanup-xts-format.R
@@ -3,8 +3,9 @@
 # Required packages ----
 library(dplyr)
 library(ggplot2)
-library(tidyr)
 library(lubridate)
+library(tibble)
+library(tidyr)
 library(xts)
 
 # Read-in functions ----
@@ -18,8 +19,8 @@ sr14_inrix_tmc <- read.csv("data/inrix/sr14-ellsworth-tmc-id.csv", stringsAsFact
 sr14_ellsworth_portal <- read.csv("data/portal/sr14-ellsworth-mp2-portal.csv", stringsAsFactors = F)
 sr14_mp193_portal <- read.csv("data/portal/sr14-mp2-ellsworth-portal.csv", stringsAsFactors = F)
 
-# SR14 EB MP 1.93 to Ellsworth ----
-## Filter and format INRIX data ----
+# SR14 EB MP 1.93 to Ellsworth ------------------------------------------------------------------
+## Filter and format EB INRIX data
 sr14_eb_tmc <- select_tmcs(sr14_inrix_tmc, "EASTBOUND")
 
 # Check confidence interval distribution
@@ -30,5 +31,38 @@ sr14_eb_conf_dist # four of the six segments have almost half confidence interva
 sr14_eb_cvalue <- cvalue_hist(sr14_inrix_raw, sr14_eb_tmc, 30)
 sr14_eb_cvalue
 
+# Calculate average hourly travel time for INRIX data
 sr14_eb_data <- select_data(sr14_inrix_raw, sr14_eb_tmc, 30, 75)
-sr14_eb_tt <- hourly_inrix_tt(sr14_eb_data, 6)
+sr14_eb_inrix_tt <- hourly_inrix_tt(sr14_eb_data, 6)
+
+# Format WSDOT/PORTAL data
+sr14_eb_portal_tt <- its_tt(sr14_mp193_portal)
+
+# create xts tibble for dygraph
+sr14_eb_tt <- its_inrix_xts("2019-02-03 00:00:00", "2019-03-03 00:00:00",
+                            sr14_eb_portal_tt, sr14_eb_inrix_tt)
+
+saveRDS(sr14_eb_tt, "data/eb_sr14_mp193_tt_xts.rds")
+
+# SR14 WB Ellsworth to MP 1.93 ----------------------------------------------------
+# Filter and format WB INRIX data
+sr14_wb_tmc <- select_tmcs(sr14_inrix_tmc, "WESTBOUND")
+
+# confidence interval distribution
+conf_dist(sr14_inrix_raw, sr14_wb_tmc)
+# half the segments have either half or almost half confidence scores of less than 30.
+
+#cvalue histo for when confidence = 30
+cvalue_hist(sr14_inrix_raw, sr14_wb_tmc, 30)
+
+# Calculate average hourly travel time for INRIX data
+sr14_wb_data <- select_data(sr14_inrix_raw, sr14_wb_tmc, 30, 75)
+sr14_wb_inrix_tt <- hourly_inrix_tt(sr14_wb_data, 6)
+
+# Format WSDOT/PORTAL data
+sr14_wb_portal_tt <- its_tt(sr14_mp193_portal)
+
+# Create xts tibble for dygraph
+sr14_wb_tt <- its_inrix_xts("2019-02-03 00:00:00", "2019-03-03 00:00:00",
+                            sr14_wb_portal_tt, sr14_wb_inrix_tt)
+saveRDS(sr14_wb_tt, "data/wb_sr14_ellsworth_xts.rds")
