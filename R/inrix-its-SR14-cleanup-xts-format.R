bb504fd6d17bd7efb6a5642493dfc82bd950e8d5
leetam <leetam@pdx.edu>
2021-03-24 19:34:58 -0700

    added i-84 comparison

--- R/inrix-its-SR14-cleanup-xts-format.R
+++ R/inrix-its-SR14-cleanup-xts-format.R
@@ -1,4 +1,4 @@
-# INRIX and WSDOT PORTAL data clean-up and xts formatting
+# INRIX and WSDOT PORTAL data clean-up and xts formatting 
 
 # Required packages ----
 library(dplyr)
@@ -66,3 +66,53 @@ sr14_wb_portal_tt <- its_tt(sr14_mp193_portal)
 sr14_wb_tt <- its_inrix_xts("2019-02-03 00:00:00", "2019-03-03 00:00:00",
                             sr14_wb_portal_tt, sr14_wb_inrix_tt)
 saveRDS(sr14_wb_tt, "data/wb_sr14_ellsworth_xts.rds")
+
+# I-84 WB 53rd to Holiday Ramp {Lloyd} -----------------------------------------------
+i84_inrix_tmc <- read.csv("data/inrix/i84-holiday-53rd-tmc-id.csv", stringsAsFactors = F)
+i84_inrix_raw <- read.csv("data/inrix/i84-holiday-53rd-inrix.csv", stringsAsFactors = F)
+
+# Filter and format WB INRIX data
+i84_wb_tmc <- select_tmcs(i84_inrix_tmc, "WESTBOUND")
+
+# confidence interval distribution
+conf_dist(i84_inrix_raw, i84_wb_tmc)
+# not bad
+
+# cvalue histo for when confidence = 30
+cvalue_hist(i84_inrix_raw, i84_wb_tmc, 30)
+
+# Calculate average hourly travel time for INRIX data
+i84_wb_data <- select_data(i84_inrix_raw, i84_wb_tmc, 30, 75)
+i84_wb_inrix_tt <- hourly_inrix_tt(i84_wb_data, 11)
+
+# Read-in and format ODOT/PORTAL data
+i84_wb_portal_data <- read.csv("data/portal/i84-53rd-holiday-portal.csv", stringsAsFactors = F)
+i84_wb_portal_tt <- its_tt(i84_wb_portal_data)
+
+# Create xts tibble for dygraph
+i84_wb_tt <- its_inrix_xts("2019-02-03 00:00:00", "2019-03-03 00:00:00",
+                           i84_wb_portal_tt, i84_wb_inrix_tt)
+saveRDS(i84_wb_tt, "data/wb_i84_53rd_holiday_xts.rds")
+
+# I-84 EB Holiday Ramp {Lloyd} to 53rd --------------------------------------------
+# Filter and format EB INRIX data
+i84_eb_tmc <- select_tmcs(i84_inrix_tmc, "EASTBOUND")
+
+# Confidence interval dist
+conf_dist(i84_inrix_raw, i84_eb_tmc)
+
+# cvalue histo for when confidence = 30
+cvalue_hist(i84_inrix_raw, i84_eb_tmc, 30)
+
+# Calculate average hourly travel time for INRIX data
+i84_eb_data <- select_data(i84_inrix_raw, i84_eb_tmc, 30, 75)
+i84_eb_inrix_tt <- hourly_inrix_tt(i84_eb_data, 11)
+
+# Read-in and format ODOT/PORTAL data
+i84_eb_portal_data <- read.csv("data/portal/i84-holiday-53rd-poral.csv", stringsAsFactors = F)
+i84_eb_portal_tt <- its_tt(i84_eb_portal_data)     
+
+# Create xts tibble for dygraph
+i84_eb_tt <- its_inrix_xts("2019-02-03 00:00:00", "2019-03-03 00:00:00",
+                           i84_eb_portal_tt, i84_eb_inrix_tt)
+saveRDS(i84_eb_tt, "data/eb_i84_holiday_53rd_xts.rds")
